c90fcb861a96ca3594c3278e47b35819b48ee513
artem122ya <artemyakushev2014@gmail.com>
2019-08-02 20:19:24 +0300

    Create new element func

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function createNewElement(tag, content = null, className = null, attributes = null) {
     const element = document.createElement(tag);
-    if (content) element.innerText = content;
+    if (content != null && typeof content == "object") element.appendChild(content); 
+    else if (content) element.innerText = content;
     if (className) element.className = className;
     if (attributes && attributes.length) attributes.map(attribute => element.setAttribute(attribute.name, attribute.value));
     return element;
@@ -11,3 +12,9 @@ function createNewElement(tag, content = null, className = null, attributes = nu
 function appendChildrenToParrent(parent, children) {
     children.map(child => parent.appendChild(child));
 }
+
+
+
+const divObj = createNewElement("div");
+const row = createNewElement("div", divObj, "row");
+document.getElementById("app").appendChild(row);
