7b17339bfcac0d109326a83adbd561e1c01ed503
artem122ya <artemyakushev2014@gmail.com>
2019-08-02 19:55:07 +0300

    Created builder functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import "./bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-console.log("asdasda"); 
+function createNewElement(tag, content = null, className = null, attributes = null) {
+    const element = document.createElement(tag);
+    if (content) element.innerText = content;
+    if (className) element.className = className;
+    if (attributes && attributes.length) attributes.map(attribute => element.setAttribute(attribute.name, attribute.value));
+    return element;
+}
+
+function appendChildrenToParrent(parent, children) {
+    children.map(child => parent.appendChild(child));
+}
