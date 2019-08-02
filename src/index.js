6b676a40db89af904c90c813b8ca03e36397d769
artem122ya <artemyakushev2014@gmail.com>
2019-08-02 20:43:50 +0300

    Created shop structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function createNewElement(tag, content = null, className = null, attributes = null) {
     const element = document.createElement(tag);
-    if (content != null && typeof content == "object") element.appendChild(content); 
+    if (content != null && typeof content == "object") appendChildrenToParrent(element, content); 
     else if (content) element.innerText = content;
     if (className) element.className = className;
     if (attributes && attributes.length) attributes.map(attribute => element.setAttribute(attribute.name, attribute.value));
@@ -13,8 +13,24 @@ function appendChildrenToParrent(parent, children) {
     children.map(child => parent.appendChild(child));
 }
 
+fetch("https://artem122ya.github.io/shop-vanilla/src/data.json")
+    .then(response => response.json())
+    .then(res => console.log(res));
 
 
-const divObj = createNewElement("div");
-const row = createNewElement("div", divObj, "row");
-document.getElementById("app").appendChild(row);
+const h5 = createNewElement("h5", "Card title", "card-title");
+const p = createNewElement("p", "text", "card-text");
+const price = createNewElement("p", "1223", "card-text");
+const a = createNewElement("a", "Buy", "btn btn-primary", [{name: "href", value: "#"}])
+
+
+const cardBody = createNewElement("div", [h5, p, price, a], "card-body");
+
+const img = createNewElement("img", null, "card-img-top", [{name: "src", value: "http://lorempixel.com/640/480"}])
+
+const card = createNewElement("div", [img, cardBody], "card");
+const col = createNewElement("div", [card], "col-4");
+
+const row = createNewElement("div", [col], "row");
+const container = createNewElement("div", [row], "container");
+document.getElementById("app").appendChild(container);
