c37e81e1b1457a82b3e6adb3d5752d0e368cdcff
artem122ya <artemyakushev2014@gmail.com>
2019-08-07 17:06:12 +0300

    New data and product class

---
+++ src/Product.js
@@ -0,0 +1,32 @@
+import { createNewElement } from "./renderFunctions";
+
+
+class Product {
+    Product(id, name, description, amount, price, pictureUrl) {
+        this.pictureUrl = pictureUrl || "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-space-gray-201706?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1539399810190";
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    createProductElement() {
+        const h5 = createNewElement("h5", this.name, "card-title");
+        const p = createNewElement("p", this.description, "card-text");
+        const priceEl = createNewElement("p", this.price, "card-text");
+        const button = createNewElement("a", "Buy", "btn btn-primary", [{name: "href", value: "#"}])
+        button.addEventListener("click", e => {
+            e.preventDefault();
+            console.log(this.id)
+        })
+    
+        const cardBody = createNewElement("div", [h5, p, priceEl, button], "card-body");
+    
+        const img = createNewElement("img", null, "card-img-top", [{name: "src", value: this.pictureUrl, width: "100%"}])
+    
+        const card = createNewElement("div", [img, cardBody], "card m-1");
+    
+        return createNewElement("div", [card], "col-4");
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
-
-function createNewElement(tag, content = null, className = null, attributes = null) {
-    const element = document.createElement(tag);
-    if (content != null && typeof content == "object") appendChildrenToParrent(element, content); 
-    else if (content) element.innerText = content;
-    if (className) element.className = className;
-    if (attributes && attributes.length) attributes.map(attribute => element.setAttribute(attribute.name, attribute.value));
-    return element;
-}
-
-function appendChildrenToParrent(parent, children) {
-    children.map(child => parent.appendChild(child));
-}
-
-function createProductElement(name, description, amount, price,
-    pictureUrl = "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-space-gray-201706?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1539399810190") {
-    const h5 = createNewElement("h5", name, "card-title");
-    const p = createNewElement("p", description, "card-text");
-    const priceEl = createNewElement("p", price, "card-text");
-    const a = createNewElement("a", "Buy", "btn btn-primary", [{name: "href", value: "#"}])
+import { renderCards } from "./renderFunctions";
 
 
-    const cardBody = createNewElement("div", [h5, p, priceEl, a], "card-body");
-
-    const img = createNewElement("img", null, "card-img-top", [{name: "src", value: pictureUrl, width: "100%"}])
-
-    return createNewElement("div", [img, cardBody], "card col-4");
-}
-
-function renderCards(cardArray) {
-
-    const row = createNewElement("div", [], "row");
-    cardArray.map(card => row.appendChild(createProductElement(card.name, card.description, card.amount, card.price)));
-    
-    const container = createNewElement("div", [row], "container");
-    document.getElementById("app").appendChild(container);
-}
 
 
-axios.get("https://artem122ya.github.io/shop-vanilla/src/data.json").then(res => renderCards(Object.values(res.data)));
+axios.get("https://artem122ya.github.io/shop-vanilla/src/data.json")
+    .then(res => renderCards(res.data.map(el => Product(el.id, el.name, el.description, el.amount, el.price, null))));
